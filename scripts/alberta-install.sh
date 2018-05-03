0e150c96efc4f8a429db3631eb9d1eadf5db2465
Carlos Teixeira <carlosteixeira.email@gmail.com>
2018-05-03 16:33:07 +0100

    upload files

---
+++ scripts/alberta-install.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/bash
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+google-chrome https://vps1.carlosteixeira.xyz/alberta --kiosk --use-fake-ui-for$
+
---
+++ scripts/alberta-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+google-chrome https://vps1.carlosteixeira.xyz/alberta --kiosk --use-fake-ui-for-media-stream
