be1cf9324f13479fb17565af1385b33b95228119
Carlos Teixeira <carlosteixeira.email@gmail.com>
2018-05-03 18:16:15 +0100

    changes on install bash script

--- scripts/alberta-install.sh
+++ scripts/alberta-install.sh
@@ -15,5 +15,5 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-google-chrome https://vps1.carlosteixeira.xyz/alberta --kiosk --use-fake-ui-for$
+google-chrome https://vps1.carlosteixeira.xyz/alberta --kiosk --use-fake-ui-for-media-stream
 
