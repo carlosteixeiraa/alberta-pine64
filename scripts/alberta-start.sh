3e0793f05860bba21659f62821b8a409c5ade64c
Carlos Teixeira <carlosteixeira.email@gmail.com>
2018-05-04 22:03:45 +0100

    changes on scripts

--- scripts/alberta-start.sh
+++ scripts/alberta-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-google-chrome https://vps1.carlosteixeira.xyz/alberta --kiosk --use-fake-ui-for-media-stream
+google-chrome https://vps1.carlosteixeira.xyz/alberta-pine64/web --kiosk --use-fake-ui-for-media-stream
