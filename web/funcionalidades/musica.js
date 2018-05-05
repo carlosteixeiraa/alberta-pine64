53dd61142595867f9ab9e3d0869d67d5e26d1556
Carlos Teixeira <carlosteixeira.email@gmail.com>
2018-05-05 14:46:02 +0100

    trying to fix a bug

--- web/funcionalidades/musica.js
+++ web/funcionalidades/musica.js
@@ -14,49 +14,56 @@ if (palavras.length == 4 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // Pesquisa 3 palavras
 if (palavras.length == 5 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // Pesquisa 4 palavras
 if (palavras.length == 6 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // Pesquisa 5 palavras
 if (palavras.length == 7 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // Pesquisa 6 palavras
 if (palavras.length == 8 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6] + ' ' + palavras[7]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // Pesquisa 7 palavras
 if (palavras.length == 9 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6] + ' ' + palavras[8] + ' ' + palavras[9]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // Pesquisa 8 palavras
 if (palavras.length == 10 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6] + ' ' + palavras[8] + ' ' + palavras[9] + ' ' + palavras[10]
   youtube();
-  window.open(linkmusica,'_blank');
-}
+  var linkem = 'https://www.youtube.com/embed/' + id;
+  var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
+  $("body").append(app);}
 // fim função
 }
