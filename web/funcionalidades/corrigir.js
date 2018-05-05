f5b051ef9cdbe7f5bcaf1382976859ac48443a5e
Carlos Teixeira <carlosteixeira.email@gmail.com>
2018-05-05 22:51:08 +0100

    fixes in musica.js

--- web/funcionalidades/corrigir.js
+++ web/funcionalidades/corrigir.js
@@ -8,4 +8,10 @@ function corrigir() {
   if (palavras[2] == "alberto") {
     palavras[2] = "alberta";
   }
+  if (palavras[3] == "alberto") {
+    palavras[3] = "alberta";
+  }
+  if (palavras[4] == "alberto") {
+    palavras[4] = "alberta";
+  }
 }
