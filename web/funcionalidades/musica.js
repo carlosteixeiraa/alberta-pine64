259c7ee3613ac5aee6aec730ef1b57925ade05cd
Carlos Teixeira <carlosteixeira.email@gmail.com>
2018-05-05 14:52:39 +0100

    trying to fix a bug

--- web/funcionalidades/musica.js
+++ web/funcionalidades/musica.js
@@ -5,7 +5,7 @@ if (palavras.length == 3 && palavras[2] !== "piano") {
   queryt = palavras[2]
   youtube();
   //window.open(linkmusica,'_blank');
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);
 }
@@ -14,7 +14,7 @@ if (palavras.length == 4 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // Pesquisa 3 palavras
@@ -22,7 +22,7 @@ if (palavras.length == 5 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // Pesquisa 4 palavras
@@ -30,7 +30,7 @@ if (palavras.length == 6 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // Pesquisa 5 palavras
@@ -38,7 +38,7 @@ if (palavras.length == 7 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // Pesquisa 6 palavras
@@ -46,7 +46,7 @@ if (palavras.length == 8 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6] + ' ' + palavras[7]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // Pesquisa 7 palavras
@@ -54,7 +54,7 @@ if (palavras.length == 9 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6] + ' ' + palavras[8] + ' ' + palavras[9]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // Pesquisa 8 palavras
@@ -62,7 +62,7 @@ if (palavras.length == 10 && palavras[2] !== "piano") {
   albertafala('É para já chefe');
   queryt = palavras[2] + ' ' + palavras[3] + ' ' + palavras[4] + ' ' + palavras[5] + ' ' + palavras[6] + ' ' + palavras[8] + ' ' + palavras[9] + ' ' + palavras[10]
   youtube();
-  var linkem = 'https://www.youtube.com/embed/' + id;
+  var linkem = 'https://www.youtube.com/embed/' + idmusica;
   var app = "<iframe style=\"display: none\" width=\"420\" height=\"315\" src=\"" + linkem + "\"" + "></iframe>"
   $("body").append(app);}
 // fim função
